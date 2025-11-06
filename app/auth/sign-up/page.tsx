29dca6b3215bfe6b313e2ef652b1453d86d0764b
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-11-06 16:58:07 +0000

    Updated signup page (Image component)

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,16 +1,12 @@
+import { BackgroundImageLoader } from '@/components/auth/background-image-loader'
 import { SignUpForm } from '@/components/sign-up-form'
 import { LandingPage } from '@/lib/supabase/services/site/landing-page-service'
 
 export default async function Page() {
   const res = await LandingPage.auth('auth_screen')
-  console.log(res)
   return (
     <div className='relative h-[88vh]'>
-      <img
-        src={res[0].image_url}
-        className='absolute w-full top-0 left-0 h-full object-cover object-center z-10'
-        alt=''
-      />
+      <BackgroundImageLoader imageUrl={res[0].image_url} />
       <div className='absolute w-full  z-30 top-52 md:pr-20'>
         <div className=' flex justify-end'>
           <SignUpForm className='w-[600px] max-w-[600px]' />
